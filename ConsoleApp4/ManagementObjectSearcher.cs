f8b6a1bb2d1f6bf303abb6985f95e56acc5b8ca6
Tural Mustafayev <mustafayevt@users.noreply.github.com>
2018-11-27 23:04:12 +0400

    Finish

---
+++ ConsoleApp4/ManagementObjectSearcher.cs
@@ -0,0 +1,14 @@
+﻿namespace ConsoleApplication1
+{
+    internal class ManagementObjectSearcher
+    {
+        private string wmipathstr;
+        private string v;
+
+        public ManagementObjectSearcher(string wmipathstr, string v)
+        {
+            this.wmipathstr = wmipathstr;
+            this.v = v;
+        }
+    }
+}
--- ConsoleApp4/Program.cs
+++ ConsoleApp4/Program.cs
@@ -1,15 +1,46 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace ConsoleApp4
+using System.Xml.Serialization;
+using System.IO;
+class Program
 {
-    class Program
+    public static void Main(string[] args)
     {
-        static void Main(string[] args)
+        List<Product> product = new List<Product>();
+        var xmlSerializer = new XmlSerializer(typeof(List<Product>));
+        if (!File.Exists("Product.xml"))
         {
+            product.Add(new Product("PS4", 500, 5));
+            product.Add(new Product("XboxOne", 600, 3));
+            FileStream file = new FileStream("Product.xml", FileMode.OpenOrCreate);
+            xmlSerializer.Serialize(file, product);
+            file.Close();
         }
+        else
+        {
+            FileStream file = new FileStream("Product.xml", FileMode.Open);
+            product = xmlSerializer.Deserialize(file) as List<Product>;
+            file.Close();
+        }
+        foreach (var item in product)
+        {
+            if (Console.ReadLine() == item.name) if (item.quantity - Convert.ToInt32(Console.ReadLine()) >= 0) item.quantity -= Convert.ToInt32(Console.ReadLine());
+                else Console.WriteLine($"Not Enough {item.name}");
+        }
+        FileStream file1 = new FileStream("Product.xml", FileMode.Open);
+        xmlSerializer.Serialize(file1, product);
     }
+}
+public class Product
+{
+    public Product() { }
+    public Product(string name, decimal price, int quantity)
+    {
+        this.name = name;
+        this.price = price;
+        this.quantity = quantity;
+    }
+    public string name { get; set; }
+    public decimal price { get; set; }
+    public int quantity { get; set; }
 }
